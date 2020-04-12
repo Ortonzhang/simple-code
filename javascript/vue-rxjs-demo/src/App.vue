bd650330bd8bb24878ccaf116184293f7757d1f9
Ortonzhang <ortonzhang@163.com>
2020-04-12 22:59:33 +0800

    倒计时效果

--- javascript/vue-rxjs-demo/src/App.vue
+++ javascript/vue-rxjs-demo/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="section">
+    <h1 class="title">{{ countdown$ }}</h1>
+    <div class="buttons">
+      <button :disabled="disabled" v-stream:click="snooze$" class="button is-success">Snooze</button>
+      <button :disabled="disabled" v-stream:click="dismiss$" class="button is-danger">Dismiss</button>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
+import { map, startWith, scan, takeWhile, concat, repeatWhen, share, takeUntil } from 'rxjs/operators';
+import { of, interval } from 'rxjs'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
+  domStreams: ['snooze$', 'dismiss$'],
+  subscriptions() {
+    const countdown$ = interval(1000).pipe(
+      startWith(5), // 设置Observable的初始值
+      scan(time => time - 1), // 类似于reduce 返回中间生成的值
+      takeWhile(time => time > 0), // 通过函数判断Observable的值 出现不满足的值就立即完成
+      concat(of('倒计时结束给你🌹！')), // 合并其他的 Observable
+      share(), // 返回一个observable 共享源Observable
+      repeatWhen(() => this.snooze$), // 返回的 Observalbe 是源 Observable 的镜像，除了 complete。 如果 notifier 发出值或 complete 通知，重新订阅源 Observable
+      takeUntil(this.dismiss$), // 如果 notifier 发出值或 complete 通知，那么输出 Observable 停止镜像源 Observable ，然后完成
+      concat(of('哈哈哈')) // 合并其他的 Observable
+    )
+    countdown$.subscribe((val) => {
+      this.disabled = !(''+val).includes('倒计时结束给你🌹！')
+    })
+    return  {
+      countdown$
+    }
   },
+  data() {
+    return {
+      disabled: false
+    }
+  }
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
--- javascript/vue-rxjs-demo/src/main.js
+++ javascript/vue-rxjs-demo/src/main.js
@@ -1,8 +1,8 @@
-import 'buefy/dist/buefy.css'
 import Vue from 'vue';
 import App from './App.vue';
 import VueRx from 'vue-rx';
-import Buefy from "buefy"
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.use(VueRx)
 Vue.use(Buefy)
