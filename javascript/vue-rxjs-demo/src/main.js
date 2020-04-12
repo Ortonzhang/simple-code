2eec23a5779f0db7fa3e32de2236543a622837b7
Ortonzhang <ortonzhang@163.com>
2020-04-12 18:39:12 +0800

    添加vue-rxjs

---
+++ javascript/vue-rxjs-demo/src/main.js
@@ -0,0 +1,14 @@
+import "buefy/lib/buefy.css"
+import Vue from 'vue';
+import App from './App.vue';
+import VueRx from 'vue-rx';
+import Buefy from "buefy"
+
+Vue.use(VueRx)
+Vue.use(Buefy)
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
