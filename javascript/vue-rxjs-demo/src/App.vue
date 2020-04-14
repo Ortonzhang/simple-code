8d00db21827b227e1e3cf8c8ec3e14d6f19851c9
Ortonzhang <ortonzhang@163.com>
2020-04-14 17:32:15 +0800

    创建tabs示例

--- javascript/vue-rxjs-demo/src/App.vue
+++ javascript/vue-rxjs-demo/src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="section">
+    <b-tabs v-model="activeTab">
+      <b-tab-item :label="people.name" :value="people.id" v-for="people in people$" :key="people.id"></b-tab-item>
+    </b-tabs>
+    <h1 class="title">{{ name$ }}</h1>
+    <img :src="images$" :alt="alt$">
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
+import { of } from 'rxjs'
+import { ajax } from 'rxjs/ajax'
+import { take, pluck, share, map, switchAll, switchMap } from 'rxjs/operators';
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      activeTab: 1
+    }
+    
   },
+  domStreams: [],
+  subscriptions() {
+    const people$ = ajax('http://localhost:3000/people').pipe(
+      pluck('response')
+    )
+
+    const activeTab$ = this.$watchAsObservable(
+      'activeTab',
+      { immediate: true }
+    ).pipe(
+      pluck('newValue')
+    )
+
+
+    const person$ = activeTab$.pipe(
+      switchMap(id => 
+        ajax(`http://localhost:3000/people/${id}`).pipe(
+          pluck('response'),
+        )
+      ),
+      share()
+    )
+
+    const name$ = person$.pipe(
+      pluck('name')
+    )
+    const images$ = person$.pipe(
+      pluck('image'),
+      map(value => `http://localhost:3000/${value}`)
+    )
+    const alt$ = name$.pipe(
+      map(value => `this is ${value}`)
+    )
+    return {
+      people$,
+      name$,
+      images$,
+      alt$,
+    }
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+nav::-webkit-scrollbar{
+  width: 0;
 }
 </style>
